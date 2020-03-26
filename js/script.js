6331ab39a468eb12a3af9a078b23afd2fd8a8e37
Blaise <baldr51@protonmail.com>
2020-03-26 23:39:59 +0100

    Fatal error system crashed lines js lines 40-42

--- js/script.js
+++ js/script.js
@@ -7,32 +7,44 @@ document.getElementById('test-button').addEventListener('click', function(){
 });
 */
 
-const titleClickHandler = function(MouseEvent){
+
+const titleClickHandler = function(Event){
   console.log('Link was clicked!');
-  console.log(MouseEvent);
+  console.log(Event);
 
-  /* remove class 'active' from all article links  */
 
+  /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');                   //where it should be removed? should query selector find "titles a.active or "titles a .active"?
+  activeLink.classList.remove('active');                                          //1. where it should be removed? should query selector find "titles a.active or "titles a .active"?
 }
 
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active '); // why article.active?
+  /* [DONE] add class 'active' to the clicked link */
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('clickedElement:', clickedElement);                                 //it's better to use this way than 'clickedElement:' + clickedElement
+  //console.log('clickedElement (with plus): ' + clickedElement);
+
+
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active ');     // 2. why article.active? only ask don't touch - it works
 
   for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');                   //where it should be removed? should query selector find "titles a.active or "titles a .active"?
+    activeArticle.classList.remove('active');                                     // 1. where it should be removed? should query selector find "titles a.active or "titles a .active"?
   }
 
-  /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [AFOOT] get 'href' attribute from the clicked link */
+  const a = document.querySelectorAll('titles')             //i'm not sure about that
+  const href = titles.getAttribute('href');
+  console.log('href')
 
-  /* add class 'active' to the correct article */
+
+  /* [AFOOT] find the correct article using the selector (value of 'href' attribute) */
+
+  /* [AFOOT] add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
