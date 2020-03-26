b23aebf6522ad591fe83e86061c9c6474b9ea639
Blaise <baldr51@protonmail.com>
2020-03-26 17:14:57 +0100

    Add listener test lines 1-9; Add handler&click listenerclick handler&click listener lines 10-30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+//  here goes a test: find test-button, listen for click, when you hear it, you execute the handler, which is to find 'titles a' and show them in the console
+/*
+document.getElementById('test-button').addEventListener('click', function(){
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
+});
+*/
+
+const titleClickHandler = function(MouseEvent){
+  console.log('Link was clicked!');
+  console.log(MouseEvent);
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
